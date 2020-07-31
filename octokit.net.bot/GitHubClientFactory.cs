f55afb029c43c071186747d42eff81682a11d358
Jan Suchotzki <jan@lernmoment.de>
2020-07-31 10:54:38 +0200

    Adds octokit.net.bot for webhook handling
    After this is integrated it allows to easily react on webhooks. This version of octokit.net.bot is my extended version. The original creator of the library does not respond to my requests.

---
+++ octokit.net.bot/GitHubClientFactory.cs
@@ -0,0 +1,68 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using Octokit.Extensions;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octokit.Bot
+{
+    public static class GitHubClientFactory
+    {
+        public static  GitHubClient CreateGitHubAppClient(GitHubOption option)
+        {
+            return GetAppClient(option, option.AppName);
+
+        }
+
+        public static async Task<InstallationContext> CreateGitHubInstallationClient(GitHubClient appClient, long installationId, string appName)
+        {
+            return await GetInstallationContext(appClient, installationId, appName);
+        }
+
+        public static async Task<InstallationContext> CreateGitHubInstallationClient(GitHubOption option,long installationId)
+        {
+            return await CreateGitHubInstallationClient(CreateGitHubAppClient(option), installationId, option.AppName);
+        }
+
+        private static async Task<InstallationContext> GetInstallationContext(GitHubClient appClient, long installationId, string appName)
+        {
+            var accessToken = await appClient.GitHubApps.CreateInstallationToken(installationId);
+
+            var installationClient = new ResilientGitHubClientFactory()
+                .Create(new ProductHeaderValue($"{appName}-Installation{installationId}"), new Credentials(accessToken.Token), new InMemoryCacheProvider());
+
+            return new InstallationContext(installationClient,accessToken);
+        }
+
+        private static GitHubClient GetAppClient(GitHubOption option, string appName)
+        {
+            // Use GitHubJwt library to create the GitHubApp Jwt Token using our private certificate PEM file
+            var generator = new GitHubJwt.GitHubJwtFactory(
+                new GitHubJwt.FilePrivateKeySource(option.PrivateKeyFileName),
+                new GitHubJwt.GitHubJwtFactoryOptions
+                {
+                    AppIntegrationId = option.AppIdentifier, // The GitHub App Id
+                    ExpirationSeconds = 600 // 10 minutes is the maximum time allowed
+                }
+            );
+            var jwtToken = generator.CreateEncodedJwtToken();
+
+            return GetGitHubClient(appName, jwtToken);
+        }
+
+        private static GitHubClient GetGitHubClient(string appName, string jwtToken)
+        {
+            return new ResilientGitHubClientFactory()
+                .Create(new ProductHeaderValue(appName), new Credentials(jwtToken, AuthenticationType.Bearer), new InMemoryCacheProvider());
+        }
+    }
+}
---
+++ octokit.net.bot/Models/GitHubOption.cs
@@ -0,0 +1,13 @@
+﻿namespace Octokit.Bot
+{
+    public class GitHubOption
+    {
+        public string WebHookSecret { get; set; }
+
+        public int AppIdentifier { get; set; }
+
+        public string PrivateKeyFileName { get; set; }
+
+        public string AppName { get; set; }
+    }
+}
